8a897dc239569a37134997041593ddcaf25fe838
asierbrizu <asierbrizu@opendeusto.es>
2021-11-12 20:01:18 +0100

    Creación ventanas inicio sesión y registro

---
+++ Proyecto Strava Grupo 11 Client/src/client/gui/VentanaIniciarSesion.java
@@ -0,0 +1,122 @@
+import java.awt.*;   
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+
+
+
+public class VentanaIniciarSesion extends JFrame {
+
+	private JPanel panelUsuario;
+	private JTextField usuario;
+	private JLabel labelUsuario = new JLabel("        Usuario: ");
+
+	private JPanel panelcontraseña;
+	private JPasswordField contraseña;
+	private JLabel labelcontraseña = new JLabel("  Contraseña: ");
+
+	private JButton aceptar;
+	private JButton crearUsuario;
+	private JPanel panelBotonera;
+
+	private JPanel panelDatos;
+
+	private JPanel panelDecorativo;
+	private JLabel labelDecorativo;
+
+	/* MAIN 
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					
+					VentanaIniciarSesion v = new VentanaIniciarSesion();
+							
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}*/
+
+
+	public VentanaIniciarSesion() {
+		setTitle("Inicio");
+		setSize(310,260);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(new GridLayout(2,1));
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setBackground(Color.WHITE);
+
+
+		panelDecorativo = new JPanel();
+		panelDecorativo.setLayout(new GridLayout(1,1));
+		panelDecorativo.setBorder(BorderFactory.createEmptyBorder(10, 0, 20, 0)); //TOC
+		labelDecorativo = new JLabel(new ImageIcon("iconos/login.png"));
+		panelDecorativo.setBackground(Color.WHITE);
+		panelDecorativo.add(labelDecorativo);
+		add(panelDecorativo);
+
+
+		panelDatos = new JPanel();
+		panelDatos.setLayout(new GridLayout(3,1));
+		panelDatos.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		panelDatos.setSize(new Dimension(100,100));
+		panelDatos.setBackground(new Color(111, 195, 179));
+		panelDatos.setPreferredSize(new Dimension(280,100));
+		add(panelDatos);
+
+		panelUsuario = new JPanel();
+		usuario = new JTextField();
+		usuario.setPreferredSize(new Dimension(100,25));
+		panelUsuario.add(labelUsuario);
+		panelUsuario.add(usuario);
+		panelUsuario.setBackground(new Color(111, 195, 179));
+		panelDatos.add(panelUsuario);
+
+		panelcontraseña = new JPanel();
+		contraseña = new JPasswordField();
+		contraseña.setPreferredSize(new Dimension(100,25));
+		panelcontraseña.add(labelcontraseña);
+		panelcontraseña.add(contraseña);
+		panelcontraseña.setBackground(new Color(111, 195, 179));
+		panelDatos.add(panelcontraseña);
+
+		panelBotonera = new JPanel();
+		aceptar = new JButton("Iniciar Sesión");
+		crearUsuario = new JButton("Crear Nuevo Usuario");
+		aceptar.setEnabled(false);
+		crearUsuario.setEnabled(true);
+		panelBotonera.add(aceptar);
+		panelBotonera.add(crearUsuario);
+		panelBotonera.setBackground(new Color(111, 195, 179));
+		panelDatos.add(panelBotonera);
+
+	
+				
+
+
+			
+
+
+
+		
+
+
+		setVisible(true);		
+	}
+
+
+
+	
+
+
+	
+
+
+
+}
+
---
+++ Proyecto Strava Grupo 11 Client/src/client/gui/VentanaRegistro.java
@@ -0,0 +1,264 @@
+
+
+
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+
+
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JSpinner;
+import com.toedter.calendar.JDateChooser;
+
+
+
+public class VentanaRegistro extends JFrame {
+	
+	   
+    private javax.swing.JButton btnGuardar;
+    private javax.swing.JButton btnCancelar;
+    private javax.swing.JLabel lblTituloVentana;
+    private javax.swing.JLabel lblNombre;
+    private javax.swing.JLabel lblEmail;
+    private javax.swing.JLabel lblContrasenia;
+    private javax.swing.JLabel lblFecha;
+    private javax.swing.JLabel lblPeso;
+    private javax.swing.JLabel lblAltura;
+    private javax.swing.JLabel lblFrecuenciaMax;
+    private javax.swing.JPanel panelCreacionUsuario;
+ //   private com.toedter.calendar.JDateChooser txtFecha1;
+    private javax.swing.JTextField txtNombre;
+    private ImageIcon imagenGuardar;
+    private JTextField textEmail;
+    private JPasswordField txtcontrasenia;
+    //private JDateChooser dateChooser;
+    
+ 	/* MAIN */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					
+					VentanaRegistro vr = new VentanaRegistro();
+							
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+  
+
+    public VentanaRegistro() {
+    	
+   
+    	
+    	
+    	imagenGuardar = new ImageIcon("img/guardar.png"); 
+
+        panelCreacionUsuario = new javax.swing.JPanel();
+        lblTituloVentana = new javax.swing.JLabel();
+        lblNombre = new javax.swing.JLabel();
+        lblEmail = new javax.swing.JLabel();
+        lblContrasenia = new javax.swing.JLabel();
+        txtNombre = new javax.swing.JTextField();
+        lblFecha = new javax.swing.JLabel();
+        lblPeso = new javax.swing.JLabel();
+        lblAltura = new javax.swing.JLabel();
+        lblFrecuenciaMax = new javax.swing.JLabel();
+        btnGuardar = new javax.swing.JButton();
+        btnGuardar.setIcon(imagenGuardar);
+        btnGuardar.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
+        btnCancelar = new javax.swing.JButton();
+
+        panelCreacionUsuario.setBackground(new java.awt.Color(51, 51, 255));
+
+        lblTituloVentana.setFont(new Font("Tahoma", Font.BOLD, 13)); 
+        lblTituloVentana.setForeground(new java.awt.Color(255, 255, 255));
+        lblTituloVentana.setText("Usuario");
+
+        lblNombre.setFont(new java.awt.Font("Tahoma", 1, 11));
+        lblNombre.setForeground(new java.awt.Color(255, 255, 255));
+        lblNombre.setText("Nombre");
+
+        lblEmail.setFont(new java.awt.Font("Tahoma", 1, 11)); 
+        lblEmail.setForeground(new java.awt.Color(255, 255, 255));
+        lblEmail.setText("Email");
+
+        lblContrasenia.setFont(new java.awt.Font("Tahoma", 1, 11)); 
+        lblContrasenia.setForeground(new java.awt.Color(255, 255, 255));
+        lblContrasenia.setText("Contrase\u00F1a");
+
+        lblFecha.setForeground(new java.awt.Color(255, 255, 255));
+        lblFecha.setText("Fecha ncto.");
+
+        lblPeso.setForeground(new java.awt.Color(255, 255, 255));
+        lblPeso.setText("Peso");
+
+        lblAltura.setForeground(new java.awt.Color(255, 255, 255));
+        lblAltura.setText("Altura");
+
+        lblFrecuenciaMax.setForeground(new java.awt.Color(255, 255, 255));
+        lblFrecuenciaMax.setText("Frecuencia cardiaca max");
+
+        btnGuardar.setText("Guardar");
+      
+
+        btnCancelar.setText("Cancelar");
+        btnCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+            	dispose();
+            	
+               
+            }
+        });
+        
+        JLabel lblFrecuenciaReposo = new JLabel();
+        lblFrecuenciaReposo.setText("Frecuencia cardiaca en reposo");
+        lblFrecuenciaReposo.setForeground(Color.WHITE);
+        lblFrecuenciaReposo.setFont(new Font("Tahoma", Font.PLAIN, 11));
+        
+        textEmail = new JTextField();
+        
+        txtcontrasenia = new JPasswordField();
+        
+        JSpinner spinnerPeso = new JSpinner();
+        
+        JSpinner spinnerAltura = new JSpinner();
+        
+        JSpinner spinnerFrecMax = new JSpinner();
+        
+        JSpinner spinnerFrecReposo = new JSpinner();
+        
+        JDateChooser dateChooser = new JDateChooser();
+        
+        javax.swing.GroupLayout gl_panelCreacionUsuario = new javax.swing.GroupLayout(panelCreacionUsuario);
+        gl_panelCreacionUsuario.setHorizontalGroup(
+        	gl_panelCreacionUsuario.createParallelGroup(Alignment.LEADING)
+        		.addGroup(Alignment.TRAILING, gl_panelCreacionUsuario.createSequentialGroup()
+        			.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.TRAILING)
+        				.addGroup(gl_panelCreacionUsuario.createSequentialGroup()
+        					.addGap(34)
+        					.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.LEADING, false)
+        						.addGroup(gl_panelCreacionUsuario.createSequentialGroup()
+        							.addComponent(lblContrasenia)
+        							.addGap(18)
+        							.addComponent(txtcontrasenia))
+        						.addGroup(gl_panelCreacionUsuario.createSequentialGroup()
+        							.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.LEADING)
+        								.addComponent(lblTituloVentana)
+        								.addComponent(lblNombre)
+        								.addComponent(lblEmail))
+        							.addGap(28)
+        							.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.LEADING)
+        								.addComponent(textEmail, GroupLayout.PREFERRED_SIZE, 136, GroupLayout.PREFERRED_SIZE)
+        								.addComponent(txtNombre, GroupLayout.PREFERRED_SIZE, 136, GroupLayout.PREFERRED_SIZE))))
+        					.addGap(103)
+        					.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.LEADING)
+        						.addGroup(gl_panelCreacionUsuario.createSequentialGroup()
+        							.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.LEADING)
+        								.addComponent(lblFecha)
+        								.addComponent(lblPeso)
+        								.addComponent(lblAltura))
+        							.addGap(62)
+        							.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.LEADING)
+        								.addComponent(dateChooser, GroupLayout.PREFERRED_SIZE, 111, GroupLayout.PREFERRED_SIZE)
+        								.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.TRAILING, false)
+        									.addComponent(spinnerAltura, Alignment.LEADING)
+        									.addComponent(spinnerPeso, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 62, Short.MAX_VALUE))))
+        						.addGroup(gl_panelCreacionUsuario.createSequentialGroup()
+        							.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.LEADING)
+        								.addComponent(lblFrecuenciaMax)
+        								.addComponent(lblFrecuenciaReposo, GroupLayout.PREFERRED_SIZE, 208, GroupLayout.PREFERRED_SIZE))
+        							.addPreferredGap(ComponentPlacement.RELATED)
+        							.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.LEADING)
+        								.addComponent(spinnerFrecReposo, GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE)
+        								.addComponent(spinnerFrecMax, GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE)))))
+        				.addGroup(gl_panelCreacionUsuario.createSequentialGroup()
+        					.addContainerGap(506, Short.MAX_VALUE)
+        					.addComponent(btnGuardar, GroupLayout.PREFERRED_SIZE, 117, GroupLayout.PREFERRED_SIZE)))
+        			.addGap(29)
+        			.addComponent(btnCancelar, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
+        			.addGap(39))
+        );
+        gl_panelCreacionUsuario.setVerticalGroup(
+        	gl_panelCreacionUsuario.createParallelGroup(Alignment.LEADING)
+        		.addGroup(gl_panelCreacionUsuario.createSequentialGroup()
+        			.addGap(40)
+        			.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.LEADING)
+        				.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.BASELINE)
+        					.addComponent(lblTituloVentana)
+        					.addComponent(lblFecha))
+        				.addComponent(dateChooser, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        			.addGap(14)
+        			.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(lblNombre)
+        				.addComponent(txtNombre, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(lblPeso)
+        				.addComponent(spinnerPeso, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE))
+        			.addGap(28)
+        			.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(lblEmail)
+        				.addComponent(lblAltura)
+        				.addComponent(textEmail, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(spinnerAltura, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE))
+        			.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.LEADING)
+        				.addGroup(gl_panelCreacionUsuario.createSequentialGroup()
+        					.addGap(24)
+        					.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.BASELINE)
+        						.addComponent(lblContrasenia)
+        						.addComponent(txtcontrasenia, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+        				.addGroup(gl_panelCreacionUsuario.createSequentialGroup()
+        					.addGap(34)
+        					.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.BASELINE)
+        						.addComponent(lblFrecuenciaMax)
+        						.addComponent(spinnerFrecMax, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE))))
+        			.addGap(18)
+        			.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(lblFrecuenciaReposo)
+        				.addComponent(spinnerFrecReposo, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE))
+        			.addPreferredGap(ComponentPlacement.RELATED, 61, Short.MAX_VALUE)
+        			.addGroup(gl_panelCreacionUsuario.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(btnGuardar, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(btnCancelar, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE))
+        			.addGap(43))
+        );
+        panelCreacionUsuario.setLayout(gl_panelCreacionUsuario);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(34, 34, 34)
+                .addComponent(panelCreacionUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(22, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(panelCreacionUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(39, 39, 39))
+        );
+
+        pack();setVisible(true);
+    }
+}
